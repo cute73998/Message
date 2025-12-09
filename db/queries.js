4935186d21852a1136bc2854c87288c2268b9b49
Lê Cường <160411972+cute73998@users.noreply.github.com>
2025-12-09 17:34:00 +0700

    add

--- db/queries.js
+++ db/queries.js
@@ -1,16 +1,16 @@
 const pool = require('./pool');
 
 async function getAllMessages(){
-    const {rows} = await pool.query('SELECT * FROM messages');
+    const {rows} = await pool.query('SELECT * FROM messages1');
     return rows;
 }
 
 async function deleteMessage(username){
-    await pool.query('DELETE FROM messages WHERE username=$1', [username]);
+    await pool.query('DELETE FROM messages1 WHERE username=$1', [username]);
 }
 
 async function insertMessage(text, username, added){
-    await pool.query('INSERT INTO messages (text, username, added) VALUES ($1, $2, $3)', [text, username, added]);
+    await pool.query('INSERT INTO messages1 (text, username, added) VALUES ($1, $2, $3)', [text, username, added]);
 }
 
 module.exports = {
